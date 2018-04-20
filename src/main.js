c0fc0a41c598a0bb1bd296fb7aec9f4852ee06db
JellyfishJoe <jellyfishjoe5@gmail.com>
2018-04-20 09:43:52 -0700

    Added drawing of square and circle

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const canvas = document.querySelector("#board canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "black";
+ctx.fillStyle = "red";
+ctx.fillRect(300, 200, 50, 50);
+ctx.strokeRect(300, 200, 50, 50);
+
+ctx.beginPath();
+ctx.arc(325, 170, 25, 0, Math.PI * 2, false);
+ctx.fill();
+ctx.stroke();
+
