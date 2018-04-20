77170a2299a84a256f06f8788988602b8de64ed8
JellyfishJoe <jellyfishjoe5@gmail.com>
2018-04-20 09:44:26 -0700

    Commented out square and circle

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector("#board canvas");
 const ctx = canvas.getContext("2d");
-
+/*
 ctx.strokeStyle = "black";
 ctx.fillStyle = "red";
 ctx.fillRect(300, 200, 50, 50);
@@ -10,4 +10,4 @@ ctx.beginPath();
 ctx.arc(325, 170, 25, 0, Math.PI * 2, false);
 ctx.fill();
 ctx.stroke();
-
+*/
