5eb0ab7b4da6e3e0b216efa51b46accf4c271e1c
JellyfishJoe <jellyfishjoe5@gmail.com>
2018-04-20 09:48:36 -0700

    Added static starfield

--- src/main.js
+++ src/main.js
@@ -11,3 +11,21 @@ ctx.arc(325, 170, 25, 0, Math.PI * 2, false);
 ctx.fill();
 ctx.stroke();
 */
+const { width: w, height: h } = canvas;
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, w, h);
+ctx.fillStyle = "#555";
+
+let x, y, radius;
+
+for (let i = 0; i < 550; i++) {
+	x = Math.random() * w;
+	y = Math.random() * h;
+	radius = Math.random() * 3;
+
+	// Draw the star!
+	ctx.beginPath();
+	ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+	ctx.fill();
+}
